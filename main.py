47ff091106d677a67bd1018cb370867f89d2e304
chen-pc <rzdchen@outlook.com>
2017-07-26 20:10:18 +0800

    â‘âémupdate gitignore

--- main.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'cc'
-
-from  scrapy.cmdline import execute
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy", "crawl", "jobbole"])
